e15125ccc315ee0c02f8c456eb5750f2e61fd84d
Abhishek <shashankmishrahjp@gmail.com>
2026-05-15 12:09:47 +0530

    Updated Java DSA solutions

--- Arrays/kadanesAlgo/maxSubarraySum.java
+++ Arrays/kadanesAlgo/maxSubarraySum.java
@@ -1,14 +1,30 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
+
             sum += nums[i];
             maxSum = Math.max(maxSum, sum);
-            if (sum < 0) sum = 0;
+
+            if (sum < 0) {
+                sum = 0;
+            }
         }
 
         return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int result = obj.maxSubArray(arr);
+
+        System.out.println("Maximum Subarray Sum = " + result);
     }
 }
--- Maths/addDigits.java
+++ Maths/addDigits.java
@@ -1,13 +1,28 @@
 class Solution {
+
     public int addDigits(int num) {
+
         while (num > 9) {
+
             int sum = 0;
+
             while (num > 0) {
                 sum += num % 10;
                 num /= 10;
             }
+
             num = sum;
         }
+
         return num;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int result = obj.addDigits(38);
+
+        System.out.println("Answer = " + result);
     }
 }
